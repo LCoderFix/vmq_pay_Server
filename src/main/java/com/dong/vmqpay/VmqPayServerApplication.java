c6525daf163a4e2a32c970fa65aebe528ad33273
liujundong <liujundong@meituan.com>
2022-04-19 01:54:45 +0800

    feat: initial commit

---
+++ src/main/java/com/dong/vmqpay/VmqPayServerApplication.java
@@ -0,0 +1,13 @@
+package com.dong.vmqpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmqPayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmqPayServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dong/vmqpay/VmqPayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dong.vmqpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmqPayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
